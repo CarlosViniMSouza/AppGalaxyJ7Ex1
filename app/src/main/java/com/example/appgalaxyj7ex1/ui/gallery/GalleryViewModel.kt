7077f23163e17d2cd0e2aaa56e1e7df1f5c7b354
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-24 01:53:03 -0300

    Project Mobile restart

---
+++ app/src/main/java/com/example/appgalaxyj7ex1/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.appgalaxyj7ex1.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
